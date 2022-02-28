afdd0ad26d8ecdffcce17cd5b086ab49460293fd
CxZMoE <cxzmoe@aliyun.com>
2022-02-28 15:04:28 +0800

    修正数字引脚

--- src/lib/block-defenition/udblock-utils.js
+++ src/lib/block-defenition/udblock-utils.js
@@ -19,4 +19,23 @@ export default (Blockly) => {
         var code = `udpi_i2c.readfrom(${addr},${num})`;
         return [code, Blockly.Python.ORDER_ATOMIC];
     }
+    Blockly.Python[`${board}_writeI2CAddr`] = function (block) {
+        Blockly.Python.definitions_['import_sensor'] = 'from udrobot.basic import *';
+        var addr = Blockly.Python.valueToCode(block, "ADDR", Blockly.Python.ORDER_ATOMIC);
+        var content = Blockly.Python.valueToCode(block, "CONTENT", Blockly.Python.ORDER_ATOMIC);
+        content = String(content).replaceAll("'","");
+
+        var code = `udpi_i2c.writeto(${addr}, bytearray([${content}]))\n`;
+        return code
+    }
+    Blockly.Python[`${board}_writeI2CToMem`] = function (block) {
+        Blockly.Python.definitions_['import_sensor'] = 'from udrobot.basic import *';
+        var addr = Blockly.Python.valueToCode(block, "ADDR", Blockly.Python.ORDER_ATOMIC);
+        var num = Blockly.Python.valueToCode(block, "NUM", Blockly.Python.ORDER_ATOMIC);
+        var content = Blockly.Python.valueToCode(block, "CONTENT", Blockly.Python.ORDER_ATOMIC);
+        content = String(content).replaceAll("'","");
+        var code = `udpi_i2c.writeto_mem(${addr},${num}, bytearray([${content}]))\n`;
+        return code
+    }
+    
 }
